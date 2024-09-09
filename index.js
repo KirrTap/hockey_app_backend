d7e76fa169a0478d5e9005daefc6c8a130db7725
Patrik Cmil <patrik.cmil@student.tuke.sk>
2024-09-09 14:56:05 +0200

    skuska vypisat kolo1

--- index.js
+++ index.js
@@ -12,18 +12,28 @@ const pool = new Pool({
     connectionString: 'postgresql://postgres:ZbSIWqZaKDAqCaEsBevyxRruLDqKtRJf@autorack.proxy.rlwy.net:42319/railway',
 });
 
-// API endpoint na zapisovanie textu do databázy
-app.post('/api/text', async (req, res) => {
-  const { text } = req.body;
+// // API endpoint na zapisovanie textu do databázy
+// app.post('/api/text', async (req, res) => {
+//   const { text } = req.body;
+//   try {
+//     const newText = await pool.query(
+//       'INSERT INTO texts (content) VALUES ($1) RETURNING *',
+//       [text]
+//     );
+//     res.json(newText.rows[0]);
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server error');
+//   }
+// });
+
+app.get('/api/kolo1', async (req, res) => {
   try {
-    const newText = await pool.query(
-      'INSERT INTO texts (content) VALUES ($1) RETURNING *',
-      [text]
-    );
-    res.json(newText.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
+    const result = await pool.query('SELECT hometeam, awayteam FROM matches WHERE kolo = $1', ['kolo1']);
+    res.json(result.rows); // Vráti záznamy z databázy
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Chyba pri získavaní údajov z databázy' });
   }
 });
 
